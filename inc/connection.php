71be5ba8412f89bbd82cba70aff8cb0f250b96eb
Boris Assoi <boris.assoi@outlook.com>
2019-12-19 12:22:16 +0000

    Mise à jour de connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -1,4 +1,3 @@
-<?php 
-	$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-    $bdd = new PDO('mysql:host=127.0.0.1;dbname=axa-gaa', 'axa', 'axa',$pdo_options);
-?>
+<?php
+$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=axa-gaa', 'axa', 'axa', $pdo_options);
